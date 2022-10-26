3307f620bc5d19e196494727fd7e630775c85c42
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2022-10-26 16:45:42 -0300

    .

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -1,17 +1,18 @@
-export default function initScrollSuave() {
-  const navItens = document.querySelectorAll("[data-scroll='smooth']")
+// export default function initScrollSuave() {
+//   const navItens = document.querySelectorAll("[data-scroll='smooth']")
 
-  navItens.forEach((itens) => {
-    itens.addEventListener("click", (e) => {
-      e.preventDefault()
-      const href = e.currentTarget.getAttribute("href")
-      const sections = document.querySelector(href)
-      console.log(sections)
+//   navItens.forEach((itens) => {
+//     itens.addEventListener("click", (e) => {
+//       e.preventDefault()
+//       const href = e.currentTarget.getAttribute("href")
+//       const sections = document.querySelector(href)
+//       console.log(sections)
 
-      sections.scrollIntoView({
-        behavior: "smooth",
-        block: "end"
-      })
-    })
-  })
-}
+//       sections.scrollIntoView({
+//         behavior: "smooth",
+//         block: "nearest",
+//         inline: "nearest"
+//       })
+//     })
+//   })
+// }
